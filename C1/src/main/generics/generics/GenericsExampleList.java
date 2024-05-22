2e667f15d12a73b66f2f0a7f1eeb15a4bf7bc0c4
joao <joaohenrique131@gmail.com>
2024-05-21 21:19:20 -0300

    Aula 1 concluida

---
+++ C1/src/main/generics/generics/GenericsExampleList.java
@@ -0,0 +1,27 @@
+package main.generics.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsExampleList {
+
+    public static void main(String[] args) {
+        //Sem Generics
+        List ListaSemGenerics = new ArrayList();
+        ListaSemGenerics.add("Elemento 1");
+        ListaSemGenerics.add(10); //Pode-se adicionar qualquer coisa.
+
+        // Com Generics
+        List<String> ListaGenerics = new ArrayList<>();
+        ListaGenerics.add("Elemento 1");
+        ListaGenerics.add("Elemento 2");
+
+        for (String elemento : ListaGenerics) {
+            System.out.println(elemento);
+        }
+        for (Object elemento : ListaSemGenerics) {
+            String str = (String) elemento;
+            System.out.println(str);
+        }
+    }
+}
