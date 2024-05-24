090de02ca199bf6682589ed15f578710d0376461
joao <joaohenrique131@gmail.com>
2024-05-24 15:28:52 -0300

    Finalização aula 3

---
+++ C2/C2/src/lista/Operacoesbasicas/Tarefa.java
@@ -0,0 +1,19 @@
+package lista.Operacoesbasicas;
+
+public class Tarefa {
+
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String toString() {
+        return descricao;
+    }
+}
