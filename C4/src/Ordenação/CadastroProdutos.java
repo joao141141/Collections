c631c145c294752d1d01e6fd46664bd8e4bf3393
joao <joaohenrique131@gmail.com>
2024-05-27 21:12:11 -0300

    Final

--- C4/src/Ordenação/CadastroProdutos.java
+++ C4/src/Ordenação/CadastroProdutos.java
@@ -1,8 +1,8 @@
 package Ordenação;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.HashSet;
 
 public class CadastroProdutos implements Comparable<Produto> {
 
@@ -23,7 +23,28 @@ public class CadastroProdutos implements Comparable<Produto> {
 
     public Set<Produto> exibirProdutoPorPreco() {
         Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
-        produtoSet.addAll(produtoSet);
+        produtosPorPreco.addAll(produtoSet);
         return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastro = new CadastroProdutos();
+
+        cadastro.adicionarProduto(01, "Limão", 12.50, 150);
+        cadastro.adicionarProduto(02, "Laranja", 10.50, 100);
+        cadastro.adicionarProduto(03, "Maçã", 15.50, 200);
+        cadastro.adicionarProduto(04, "Banana", 8.50, 50);
+        cadastro.adicionarProduto(05, "Uva", 20.50, 300);
+        cadastro.adicionarProduto(06, "Pera", 18.50, 250);
+
+        System.out.println("Produtos por nome: " + cadastro.exibirProdutosPorNome());
+
+        System.out.println("Produtos por preço: " + cadastro.exibirProdutoPorPreco());
+
+    }
+
+    @Override
+    public int compareTo(Produto o) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
